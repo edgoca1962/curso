889b7056f44f4bc8d88b7cde7178b8b8840c7ada
Edwin Gonzalez <edgoca1962@gmail.com>
2024-09-05 17:44:47 -0600

    Leccion 2

---
+++ wp-content/themes/leccion02/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/**
+ * Functions Theme
+ * 
+ * @package MYDOMANIN
+ * 
+ */
+
+use MYDOMAIN\Modules\Core\CoreController;
+
+if (!defined('MYDOMAIN_DIR_PATH')) {
+   define('MYDOMAIN_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+if (!defined('MYDOMAIN_DIR_STYLE')) {
+   define('MYDOMAIN_DIR_STYLE', untrailingslashit(get_stylesheet_uri()));
+}
+if (!defined('MYDOMAIN_DIR_URI')) {
+   define('MYDOMAIN_DIR_URI', untrailingslashit(get_template_directory_uri()));
+}
+if (!defined('MYDOMAIN_POST_THUMBNAIL_URI')) {
+   define('MYDOMAIN_POST_THUMBNAIL_URI', untrailingslashit(get_the_post_thumbnail_url()));
+}
+if (!defined('MYDOMAIN_MODULOS')) {
+   define('MYDOMAIN_MODULOS', []);
+}
+require_once MYDOMAIN_DIR_PATH . '/modules/core/autoloader.php';
+
+if (!function_exists('MYDOMAIN_get_theme_instance')) {
+   function MYDOMAIN_get_theme_instance()
+   {
+      CoreController::get_instance();
+   }
+   MYDOMAIN_get_theme_instance();
+}
+function MYDOMAIN_register_scripts_styles()
+{
+   wp_enqueue_style('styles', MYDOMAIN_DIR_STYLE, array(), microtime(), 'all');
+   wp_enqueue_script('scripts', MYDOMAIN_DIR_URI . '/assets/main.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'MYDOMAIN_register_scripts_styles');
---
+++ wp-content/themes/leccion02/modules/core/corecontroller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MYDOMAIN\Modules\Core;
+
+/**
+ * 
+ * Clase Core
+ * 
+ * @package MYDOMANIN
+ * 
+ */
+
+class CoreController
+{
+   use Singleton;
+   private $atributos;
+   private function __construct()
+   {
+      $this->atributos = [];
+   }
+   public function set_atributo($parametro, $valor)
+   {
+      $this->atributos[$parametro] = $valor;
+   }
+   public function get_atributo($parametro)
+   {
+      return $this->atributos[$parametro];
+   }
+   public function get_datos()
+   {
+      $datos = [];
+
+      $datos['post_type'] = get_post_Type();
+      $datos['post_id'] = get_the_ID();
+      $datos['body'] = 'bg-dark bg-gradient text-white';
+
+      $this->atributos = $datos;
+
+      return $this->atributos;
+   }
+}
