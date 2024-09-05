d48848db015ce47ec50c86115b3f8c0b8d4a1cab
Edwin Gonzalez <edgoca1962@gmail.com>
2024-09-05 17:50:20 -0600

    Leccion 7

---
+++ wp-content/themes/leccion07/modules/core/view/core-cambio-clave.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 
+ * Planitlla para cambio contraseña.
+ * 
+ * @package MYDOMAIN
+ * 
+ */
+
+
+?>
+
+<section class="d-flex justify-content-center pt-5">
+   <div class="col-md-6">
+      <form id="cambiar_clave" class="row g-3 needs-validation float" novalidate>
+         <div id="clave_actual" class="d-flex form-floating mb-3 border-bottom">
+            <input type="password" class="form-control bg-transparent border-0 shadow-none text-white" id="clave_actual" name="clave_actual" placeholder="contraseña" required>
+            <label for="clave_actual">Contraseña </label>
+            <span class="mt-4" style="font-size: 70%;"><i id="ver_clave_actual" class="fa-solid fa-eye"></i></i></span>
+         </div>
+         <div id="clave_nueva" class="d-flex form-floating mb-3 border-bottom">
+            <input type="password" class="form-control bg-transparent border-0 shadow-none text-white" id="clave_nueva" name="clave_nueva" placeholder="contraseña" required>
+            <label for="clave_nueva">Contraseña nueva</label>
+            <span class="mt-4" style="font-size: 70%;"><i id="ver_clave_nueva" class="fa-solid fa-eye"></i></i></span>
+         </div>
+         <div id="confirmar_clave" class="d-flex form-floating mb-3 border-bottom">
+            <input type="password" class="form-control bg-transparent border-0 shadow-none text-white" id="confirmar_clave" name="confirmar_clave" placeholder="contraseña" required>
+            <label for="confirmar_clave">Comprobación</label>
+            <span class="mt-4" style="font-size: 70%;"><i id="ver_clave_nueva" class="fa-solid fa-eye"></i></i></span>
+         </div>
+         <input type="hidden" name="action" value="cambiar_clave">
+         <input type="hidden" name="nonce" value="<?php echo wp_create_nonce('cambiar_clave') ?>">
+         <input type="hidden" name="endpoint" value="<?php echo admin_url('admin-ajax.php') ?>">
+      </form>
+   </div>
+</section>
